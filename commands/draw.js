6a428386434b664c573f30ad4097f2a3cba47989
GaryStens <109638433+GaryStens@users.noreply.github.com>
2022-07-27 12:26:01 +0800

    Updated draw.js
    Shows a whole new invalid arguments message

--- commands/draw.js
+++ commands/draw.js
@@ -7,7 +7,17 @@ module.exports = {
   async minecraft (handler) {
     const name = handler.args.join(' ').replaceAll(/§r/g, '')
     const fullpath = path.join(__dirname, '../images', name)
-
+    const a = handler.args[0]
+    
+    switch (a) {
+      case 'list':
+        handler.bot.core?.run('/tellraw @a ["",{"text":"Image Files List","color":"light_purple"},{"text":":","color":"gray"},{"text":"\n\n"},{"text":"amogus.jpg\nship.jpg","color":"red"},{"text":"\n"},{"text":"cow.jpg","color":"dark_red"},{"text":"\n"},{"text":"youtube-invalid-video.jpg","color":"red"},{"text":"\n"},{"text":"glass-of-water.jpg","color":"dark_red"}]')
+        break
+      case '':
+        handler.bot.core?.run(`/tellraw @a ["",{"text":"YBotV2 ","color":"light_purple"},{"text":"\u25ba ","color":"dark_gray"},{"text":"Invaid arguments!","color":"gray"},{"text":"\n\n"},{"text":"Usage ","color":"red"},{"text":"- ","color":"dark_gray"},{"text":"${config.prefixes}draw [list, <file>]","color":"red"}]`}
+        break
+    }
+    
     const image = await sharp(fullpath)
 
     const metadata = await image
