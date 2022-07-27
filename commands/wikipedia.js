b24c183f9dafa32f0b086c3d83965988b290c960
GaryStens <109638433+GaryStens@users.noreply.github.com>
2022-07-27 13:03:21 +0800

    Updated wikipedia.js

--- commands/wikipedia.js
+++ commands/wikipedia.js
@@ -4,6 +4,13 @@ module.exports = {
   name: 'wikipedia',
   async minecraft (handler) {
     const topic = handler.args.join(' ')
+    const a = handler.args[0]
+    const empty = handler.args[0]
+    
+    switch (a) {
+      case empty:
+        handler.bot.core?.run('/tellraw @a ["",{"text":"YBotV2 ","color":"light_purple"},{"text":"\u25ba ","color":"dark_gray"},{"text":"Provide a topic name.","color":"gray"}]')
+    }
 
     const page = await wikipedia.page(topic)
 
