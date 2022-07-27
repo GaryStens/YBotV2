28799829b8aa40e569e880d8f6ec6071436865f0
GaryStens <109638433+GaryStens@users.noreply.github.com>
2022-07-27 12:31:54 +0800

    Updated opme.js
    Newer message!

--- commands/opme.js
+++ commands/opme.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: 'opme',
   minecraft (handler) {
-    handler.bot.core?.run(`/execute run op ${handler.selector}`)
+    setTimeout(() => handler.bot.core?.run('/execute run op ' + handler.selector), 100)
+    setTimeout(() => handler.bot.core?.run('/tellraw @a ["",{"text":"YBotV2 ","color":"light_purple"},{"text":"\u25ba ","color":"dark_gray"},{"text":"Success!","color":"green"}]'), 200)
   }
 }
