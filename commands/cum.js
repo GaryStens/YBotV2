cd9fc568398dc9aa713c9d63a8fd71f4747cada9
GaryStens <109638433+GaryStens@users.noreply.github.com>
2022-07-27 12:05:55 +0800

    Updated cum.js
    Made a new message

--- commands/cum.js
+++ commands/cum.js
@@ -3,6 +3,7 @@ const { selectUsername } = require('../util/player')
 module.exports = {
   name: 'cum',
   minecraft (handler) {
-    handler.bot.core?.run(`minecraft:give ${selectUsername(handler.bot, handler.sender)} potion{display:{Name:'{"text":"Cum","italic":false}'},CustomPotionColor:16777215}`)
+    setTimeout(() => handler.bot.core?.run(`minecraft:give ${selectUsername(handler.bot, handler.sender)} potion{display:{Name:'{"text":"Cum","italic":false}'},CustomPotionColor:16777215}`), 100)
+    setTimeout(() => handler.bot.core?.run(`/tellraw @a ["",{"text":"YBotV2 ","color":"light_purple"},{"text":"\u25ba ","color":"dark_gray"},{"text":"Happy cumming!","color":"gray"}]`), 200)
   }
 }
